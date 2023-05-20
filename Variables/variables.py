7b67e279723b64b971a372dd19ce84b659b8ee91
Luciano Griffa <lucianogriffa@protonmail.com>
2023-05-20 18:32:02 -0300

    Variables Folder & variables.py

---
+++ Variables/variables.py
@@ -0,0 +1,15 @@
+# Defining a variable with camelCase
+fullName = "Luciano Griffa"
+
+# Defining a variable with snake_case
+full_name = "Luciano Griffa"
+
+# Concatenate with +
+welcome = "Hello " + " ¿How are you?"
+
+# Concatenate with f-strings
+welcome = f"Hello {full_name} ¿How are you?"
+
+# Membership operators (in / not in)
+print("Luciano" in welcome) #true
+print ("Luciano" not in welcome) #false

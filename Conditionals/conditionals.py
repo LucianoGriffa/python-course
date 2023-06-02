28cf7e7298da397ac87dd8c6fd0280d871d4114d
G3kSec <lucianogriffa@protonmail.com>
2023-06-02 02:29:17 -0300

    conditionals

---
+++ Conditionals/conditionals.py
@@ -0,0 +1,10 @@
+# Conditionals - Condicionales
+# if - elif - else
+
+age = 20
+if age < 18:
+  print("Eres menor de edad")
+elif age >= 18 and age < 65:
+  print("Eres adulto")
+else:
+  print("Eres un adulto mayor")

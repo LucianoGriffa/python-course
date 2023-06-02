d4fb21341f5534eb94d2e6a4a0aaf19376e0e1c2
G3kSec <lucianogriffa@protonmail.com>
2023-06-02 01:41:43 -0300

    arithmeticOperators

---
+++ Operators/arithmetic_operators.py
@@ -0,0 +1,15 @@
+# Arithmetic Operators - Operadores Aitméticos
+# Addition - Suma (+)
+addition = 2 + 2
+# Subtraction - Resta (-)
+subtraction = 4 - 2
+# Multiplication - Multiplicación (*)
+multiplication = 2 * 3
+# Division - División (/)
+division = 10 / 5
+# Exponentiation - Exponenciación (**)
+exponentiation = 15 ** 5
+# Floor division - División baja (//)
+floor_division = 12 // 6
+# Modulus - Módulo (%)
+modulus = 18 % 6

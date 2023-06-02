cffc93eddac6464cdd5c19c28546846e5219685e
G3kSec <lucianogriffa@protonmail.com>
2023-06-02 02:06:46 -0300

    comparisonOperators

---
+++ Operators/comparison_operators.py
@@ -0,0 +1,13 @@
+# Comparison Operators - Operadores de Comparación
+# Equal - Igual
+x == y
+# Not equal - No igual
+x != y
+# Greater than - Mayor que
+x > y
+# Less than - Menor que
+x < y
+# Greater than or equal to - Mayor que o igual que
+x >= y 
+# Less than or equal to - Menor que o igual que
+x <= y

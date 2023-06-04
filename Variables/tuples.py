ed3d7747c97dd7d55445290bacc5a339cd4cb5f4
G3kSec <lucianogriffa@protonmail.com>
2023-06-04 16:35:05 -0300

    Tuples Variables 2.0

---
+++ Variables/tuples.py
@@ -0,0 +1,12 @@
+# Tuples - Tuplas
+
+# Create tuple with tuple().
+tuple_one = tuple(['One', 'Two'])
+
+# Creating a tuple without parentheses from multiple data.
+tuple_two = 'One', 'Two', 'Three'
+
+# Creating a tuple without parentheses from a single data.
+tuple_three = 'One',
+
+print(tuple_one, tuple_two, tuple_three)

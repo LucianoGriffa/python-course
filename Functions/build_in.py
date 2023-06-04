5df12da9aefa899b73b7d1ea43c681f245eeac11
G3kSec <lucianogriffa@protonmail.com>
2023-06-04 18:49:37 -0300

    Integrated Functions

---
+++ Functions/build_in.py
@@ -0,0 +1,21 @@
+
+
+numbers = [4, 7, 43, 20]
+# Finding the largest number in a list
+number_most_largest = max(numbers)
+# Finding the smallest number in a list
+number_most_smallest = min(numbers)
+
+# Rounding to 6 decimal places
+number = round(12.3456, 2) # round(number, decimal quantity)
+
+# Return False -> 0  or [] or () or empty or False or none
+result_bool = bool(2)
+
+# Return True, if all values are true
+result_all = all([234, "True", [23654]])
+
+# Sums all the values of an iterable
+total_sum = sum(numbers)
+
+print(total_sum)
